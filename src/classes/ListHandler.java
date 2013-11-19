2ad71288d0da8cfa5ac198de820557a4eb7548e9
Fabi <mrehris@gmail.com>
2013-11-19 15:20:21 +0100

    Problem found in ListHandler for sort (sortList wasn't inizialised)//thannks to Constantin for help

--- src/classes/ListHandler.java
+++ src/classes/ListHandler.java
@@ -1,34 +1,39 @@
 package classes;
+
 import java.util.Collection;
 import java.util.LinkedList;
+
 public class ListHandler {
 	LinkedList<LinkedList<Pattern>> patternCandidates;
-	LinkedList<Pattern> sortetList;
+	// TODO: In speicher erst inizialisieren wen es gebraucht wird;
+	LinkedList<Pattern> sortetList = new LinkedList<Pattern>();
 
-	LinkedList<Pattern> M1Sorted;
-	LinkedList<Pattern> M2Sorted;
-	LinkedList<Pattern> M3Sorted;
+	// TODO: In Speicher erst inizialisieren wen es gebraucht wird;
+	LinkedList<Pattern> M1Sorted = new LinkedList<Pattern>();
+	LinkedList<Pattern> M2Sorted = new LinkedList<Pattern>();
+	LinkedList<Pattern> M3Sorted = new LinkedList<Pattern>();
 	LinkedList<Pattern> patternM1 = new LinkedList<Pattern>();
 
-	public ListHandler(LinkedList<LinkedList<Pattern>> patternCandidates)
-	{
+	public ListHandler(LinkedList<LinkedList<Pattern>> patternCandidates) {
 		this.patternCandidates = patternCandidates;
 
 	}
-	
-	//		sort pattern and add them in Mi in Pattern
+
+	// sort pattern and add them in Mi in Pattern
 	public void sortPatternCandidatesM(int mType) {
-		//		sort for m1
+		// sort for m1
 		sort(mType);
-		//		add sortetList to MiSorted
-		for (Pattern currentList : sortetList) 
-		{
+		// add sortetList to MiSorted
+		for (Pattern currentList : sortetList) {
 			switch (mType) {
-			case 1:M1Sorted.add(currentList);
+			case 1:
+				M1Sorted.add(currentList);
 				break;
-			case 2:M2Sorted.add(currentList);
+			case 2:
+				M2Sorted.add(currentList);
 				break;
-			case 3:M3Sorted.add(currentList);
+			case 3:
+				M3Sorted.add(currentList);
 				break;
 
 			default:
@@ -39,52 +44,63 @@ public class ListHandler {
 	}
 
 	private void sort(int mType) {
-//		TODO: adding pattern from patternCandidates to Linkedlist<Pattern> is not possible
+		// TODO: adding pattern from patternCandidates to Linkedlist<Pattern> is
+		// not possible
 		for (LinkedList<Pattern> PatternLink : patternCandidates) {
-			sortetList.addAll(PatternLink); //throws Nullpointer Exeption becouse foundpattern semms empty
+			if (patternCandidates.isEmpty()) {
+				System.out.println("patternCandidates ist lehr");
+			} else {
+				sortetList.addAll(PatternLink); // throws Nullpointer Exeption
+				// becouse foundpattern semms
+				// empty
+			}
+
 		}
-//		TODO: sort sortedList
+		// TODO: sort sortedList
 	}
 
-//	A Version of Felix (not working, same Problem withe NullpointerExeption)
-	//	public void sortPatternCandidatesM(int mType) {
+	// A Version of Felix (not working, same Problem withe NullpointerExeption)
+	// public void sortPatternCandidatesM(int mType) {
 	//
-	//		for (LinkedList<Pattern> currentLinked : patternCandidates) {
-	//			for (Pattern pattern : currentLinked) {
-	//				patternM1.add(pattern);
-	//			}
-	//		}
+	// for (LinkedList<Pattern> currentLinked : patternCandidates) {
+	// for (Pattern pattern : currentLinked) {
+	// patternM1.add(pattern);
+	// }
+	// }
 	//
-	//		M1Sorted.add(patternM1.getFirst());
+	// M1Sorted.add(patternM1.getFirst());
 	//
-	//		for(int i = 1; i < patternM1.size(); i++){
+	// for(int i = 1; i < patternM1.size(); i++){
 	//
-	//			for(int j = 0; j < M1Sorted.size(); j++){
+	// for(int j = 0; j < M1Sorted.size(); j++){
 	//
-	//				if(patternM1.get(i).getM_Value(mType) < M1Sorted.get(j).getM_Value(mType)) 
-	//					M1Sorted.add(j, patternM1.get(i));
-	//				else M1Sorted.addLast(patternM1.get(i));
-	//			}
-	//		}
+	// if(patternM1.get(i).getM_Value(mType) <
+	// M1Sorted.get(j).getM_Value(mType))
+	// M1Sorted.add(j, patternM1.get(i));
+	// else M1Sorted.addLast(patternM1.get(i));
+	// }
+	// }
 	//
-	//	}
+	// }
 
-	public void cleanSortedLists()
-	{
-		//		clean M1
-		if(M1Sorted.size()<=100)
+	public void cleanSortedLists() {
+		// clean M1
+		if (M1Sorted.size() <= 100)
 			;
 		else
-			M1Sorted =  (LinkedList<Pattern>) M1Sorted.subList(M1Sorted.size()-100, M1Sorted.size());
-		//		clean M2
-		if(M2Sorted.size()<=100)
+			M1Sorted = (LinkedList<Pattern>) M1Sorted.subList(
+					M1Sorted.size() - 100, M1Sorted.size());
+		// clean M2
+		if (M2Sorted.size() <= 100)
 			;
 		else
-			M2Sorted = (LinkedList<Pattern>) M2Sorted.subList(M2Sorted.size()-100, M2Sorted.size());
-		//		clean M3
-		if(M3Sorted.size()<=100)
+			M2Sorted = (LinkedList<Pattern>) M2Sorted.subList(
+					M2Sorted.size() - 100, M2Sorted.size());
+		// clean M3
+		if (M3Sorted.size() <= 100)
 			;
 		else
-			M3Sorted = (LinkedList<Pattern>) M3Sorted.subList(M3Sorted.size()-100, M3Sorted.size());
+			M3Sorted = (LinkedList<Pattern>) M3Sorted.subList(
+					M3Sorted.size() - 100, M3Sorted.size());
 	}
 }
