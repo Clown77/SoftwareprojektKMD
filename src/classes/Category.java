3ccf9eec81ff4ad34a192f98716d29a294cc152f
Mazze <M.Hewelt@gmx.de>
2014-01-02 17:33:27 +0100

    Categories fertig programmiert. Ist aber noch nicht in der Main eingebaut und auch noch nicht getestet. Kategorien mergen müsste jetzt aber auch funktionieren. Wenn wir den Stuff getestet haben, fehlt dann also nur noch das Windowing. Aber bevor wir das machen, sollten wir erstmal wirklich einen Text schreiben, bei dem wir genau wissen was raus kommt und alle Fälle mal durchtesten. Habe die Main auch wieder angepasst und die Sachen in Funktionen gepackt.

--- src/classes/Category.java
+++ src/classes/Category.java
@@ -6,11 +6,11 @@ public class Category
 {
 	// clique will contain the bidirectional Pattern
 	Pattern clique;
-	
+
 	// We have 2-Cliques. This Words are the two words of the clique
 	String cliqueFirst;
 	String cliqueSecond;
-	
+
 	// The category contains the clique and all Words that fit to that category
 	LinkedList<String> category;
 
@@ -18,110 +18,168 @@ public class Category
 	public Category(Pattern biDirectionalPattern)
 	{
 		clique = biDirectionalPattern;
-		
-		cliqueFirst = clique.pattern.getFirst(); 
+
+		cliqueFirst = clique.pattern.getFirst();
 		cliqueSecond = clique.pattern.getLast();
-		
+
 		category.addAll(clique.pattern);
 	}
-	
-	/** @description will search through the whole List of all Patterns and add all words to the category that fulfill the conditions to be part of
-	 * 				 the category. These conditions are: 
-	 * 				(1) At least unidirectional connected to all words from the clique and 
-	 * 				(2) bidirectional connected to at least one word of the clique
-	 *  
-	 * @param finalPatternList is the List of all Pattern that were collected doing the M1, M2 and M3 measurements
-	 * @param allWords contains all the words from the finalPatternList as Strings
+
+	/**
+	 * @description will search through the whole List of all Patterns and add
+	 *              all words to the category that fulfill the conditions to be
+	 *              part of the category. These conditions are: (1) At least
+	 *              unidirectional connected to all words from the clique and
+	 *              (2) bidirectional connected to at least one word of the
+	 *              clique
 	 * 
-	 * TODO Die boolean fulfillsConditions braucht man wegen dem Break eigentlich nicht. Tests schreiben, ob es auch ohne geht.
+	 * @param finalPatternList
+	 *            is the List of all Pattern that were collected doing the M1,
+	 *            M2 and M3 measurements
+	 * @param allWords
+	 *            contains all the words from the finalPatternList as Strings
+	 * 
+	 *            TODO Die boolean fulfillsConditions braucht man wegen dem
+	 *            Break eigentlich nicht. Tests schreiben, ob es auch ohne geht.
 	 */
-	public void fillCategory(LinkedList<Pattern> finalPatternList, LinkedList<String> allWords)
+	public void fillCategory(LinkedList<Pattern> finalPatternList,
+			LinkedList<String> allWords)
 	{
 		boolean fulfillsConditions = true;
-		
+
 		for (String word : allWords)
 		{
 			fulfillsConditions = true;
-			
-			// test if the word is unidirectional connected to all words of the clique
-			if(!uniDirectionalToAll(word, finalPatternList)) 
+
+			// test if the word is unidirectional connected to all words of the
+			// clique
+			if (!uniDirectionalToAll(word, finalPatternList))
 			{
 				fulfillsConditions = false;
 				break;
 			}
-			
-			// test if the word is bidirectional connected to all words of the clique
-			if(!biDirectionalToOne(word, finalPatternList))
+
+			// test if the word is bidirectional connected to all words of the
+			// clique
+			if (!biDirectionalToOne(word, finalPatternList))
 			{
 				fulfillsConditions = false;
 				break;
 			}
-			
+
 			// if the word passed all tests, add it to the category
-			if(fulfillsConditions) category.add(word);
-		}	
+			if (fulfillsConditions)
+				category.add(word);
+		}
 	}
-	
-	/** @description Tests if the given word is unidirectional connected to all words in the clique
+
+	/**
+	 * @description Tests if the given word is unidirectional connected to all
+	 *              words in the clique
 	 */
-	private boolean uniDirectionalToAll(String word, LinkedList<Pattern> finalPatternList)
+	private boolean uniDirectionalToAll(String word,
+			LinkedList<Pattern> finalPatternList)
 	{
-		// If the word is unidirectional connected, there has to be any connection to the first word of the clique...
-		if( !(hasArc(word, cliqueFirst, finalPatternList) || hasArc(cliqueFirst, word, finalPatternList)) ) return false;
-		
+		// If the word is unidirectional connected, there has to be any
+		// connection to the first word of the clique...
+		if (!(hasArc(word, cliqueFirst, finalPatternList) || hasArc(
+				cliqueFirst, word, finalPatternList)))
+			return false;
+
 		// ...and any connection to the second word of the clique.
-		if( !(hasArc(word, cliqueSecond, finalPatternList) || hasArc(cliqueSecond, word, finalPatternList)) ) return false;
-		
+		if (!(hasArc(word, cliqueSecond, finalPatternList) || hasArc(
+				cliqueSecond, word, finalPatternList)))
+			return false;
+
 		return true;
 	}
 
-	
-	/** @description Tests if the given word is bidirectional connected to at least one word in the clique
+	/**
+	 * @description Tests if the given word is bidirectional connected to at
+	 *              least one word in the clique
 	 */
-	private boolean biDirectionalToOne(String word, LinkedList<Pattern> finalPatternList)
+	private boolean biDirectionalToOne(String word,
+			LinkedList<Pattern> finalPatternList)
 	{
 		boolean bidirectional = false;
-		
+
 		// there has to be a bidirectional connection to the first...
-		if( hasArc(word, cliqueFirst, finalPatternList) && hasArc(cliqueFirst, word, finalPatternList) ) bidirectional = true;
-		
+		if (hasArc(word, cliqueFirst, finalPatternList)
+				&& hasArc(cliqueFirst, word, finalPatternList))
+			bidirectional = true;
+
 		// ... or the second word of the clique.
-		if( hasArc(word, cliqueSecond, finalPatternList) && hasArc(cliqueSecond, word, finalPatternList) ) bidirectional = true;
-		
+		if (hasArc(word, cliqueSecond, finalPatternList)
+				&& hasArc(cliqueSecond, word, finalPatternList))
+			bidirectional = true;
+
 		return bidirectional;
 	}
-	
-	
-	/** @description Tests if there is a connection between the Strings "from" to "to".
+
+	/**
+	 * @description Tests if there is a connection between the Strings "from" to
+	 *              "to".
 	 */
-	private boolean hasArc(String from, String to, LinkedList<Pattern> finalPatternList)
+	private boolean hasArc(String from, String to,
+			LinkedList<Pattern> finalPatternList)
 	{
 		// First we create a FakePattern...
 		LinkedList<String> ghostPattern = new LinkedList<String>();
 		ghostPattern.addFirst(from);
 		ghostPattern.addLast(to);
-		
+
 		Pattern fakePattern = new Pattern(ghostPattern);
-		
+
 		// Then we test, if this Pattern already exists in the finalPatternList
 		return finalPatternList.contains(fakePattern);
 	}
-	
-	/** @description Tests if the offered Category contains >= 50 % of this category. If yes, it will merge both categories and return true, else returns false. 
+
+	/**
+	 * @description Tests if the offered Category contains >= 50 % of this
+	 *              category. If yes, it will merge both categories and return
+	 *              true, else returns false.
 	 */
-	public boolean offerCategory(Pattern offeredCategory)
+	public boolean offerCategory(LinkedList<String> offeredCategory)
 	{
+		LinkedList<String> differentWords = new LinkedList<String>();
+		double wordCount = offeredCategory.size();
+		double identicalWords = 0;
 		
+		// counts how many words of the given category are contained in our category 
+		for (String word : offeredCategory)	
+		{
+			if(containsWord(word)) identicalWords++;
+			else differentWords.add(word);
+		}
+		
+		// calculate the relation between both categories
+		double ratio = identicalWords/wordCount;
+		
+		if(ratio >= 0.5)
+		{
+			category.addAll(differentWords);
+			return true;
+		}
+		
+		return false;
 	}
-	
-	/** @description Returns whether this category contains the given word or not.
+
+	/**
+	 * @description Returns whether this category contains the given word or not.
 	 */
-	public boolean containsWord(String word)
+	private boolean containsWord(String word)
 	{
-		
+		for (String categoryWord : category)
+		{
+			if (categoryWord.equals(word))
+				return true;
+		}
+
+		return false;
 	}
-	
-	/** @description returns a String-representation of the Category
+
+	/**
+	 * @description returns a String-representation of the Category
 	 */
 	public String toString()
 	{
